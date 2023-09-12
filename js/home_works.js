2e5703e88aae768d4a8cbd62db9b499e7071b051
Akmaral <akmaral.minbaeva1921@gmail.com>
2023-09-12 16:44:21 +0600

    2 commit

--- js/home_works.js
+++ js/home_works.js
@@ -18,23 +18,72 @@ gmailButton.onclick = () => {
 
 const parentBlock = document.querySelector('.parent_block')
 const childBlock = document.querySelector('.child_block')
-let num = 0
-childBlock.style.left = num
+// let num = 0
+// childBlock.style.left = num
+// const move = () => {
+//     num++
+//     childBlock.style.left = `${num}px`
+//     if (num < 448) {
+//         num++
+//         childBlock.style.left = `${num}px`
+//         animation()
+
+//     }
+// }
+// function animation() {
+//     setTimeout(move, 10)
+// }
+// animation()
+
+let positionX = 0
+let positionY = 0
 
 const move = () => {
-    num++
-    childBlock.style.left = `${num}px`
-    if (num < 448) {
-        animation()
-
+    if (positionX < 448 && positionY === 0) {
+        positionX++
+        childBlock.style.left = `${positionX}px`
+        setTimeout(move, 1)
+    } else if (positionX === 448 && positionY < 448) {
+        positionY++
+        childBlock.style.top = `${positionY}px`
+        setTimeout(move, 1)
+    } else if (positionX > 0 && positionY === 448) {
+        positionX--
+        childBlock.style.left = `${positionX}px`
+        setTimeout(move, 1)
+    } else if (positionX === 0 && positionY > 0) {
+        positionY--
+        childBlock.style.top = `${positionY}px`
+        setTimeout(move, 1)
     }
 }
+move()
 
-function animation() {
-    setTimeout(move, 10)
+
+const minuts = document.querySelector('#minutesS')
+const start = document.querySelector('#start')
+const stop = document.querySelector('#stop')
+const reset = document.querySelector('#reset')
+let num = 0
+
+start.onclick = () => {
+    const go = setInterval(() => {
+        if (num < 59) {
+            num++
+            minuts.innerHTML = num
+        }
+    }, 1000)
+    stop.onclick = () => {
+        clearInterval(go)
+    }
+}
+reset.onclick = () => {
+    num = 0
+    minuts.innerHTML = num
+
 }
 
-animation()
+
 
 
 
